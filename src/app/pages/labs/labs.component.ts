72a9a9295679c6eae970403dbf22c37cc2b4fb98
PouDDuoP <kevinalvarado.ag@gmail.com>
2024-01-22 23:33:19 -0400

    Add Event binding: keydown
    Se realizan pruebas de los eventos y captura de los mismos.

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -33,6 +33,11 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event) {
-    console.log(event)
+    console.log(event);
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
   }
 }

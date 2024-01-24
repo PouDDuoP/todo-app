61b719a1f56a92dca52e644b774652552a4377be
PouDDuoP <kevinalvarado.ag@gmail.com>
2024-01-24 19:01:47 -0400

    Add Uso de ngFor add events
    Se agregaron los eventos para actualizar mediante angular el array con las tareas, se creo uno para agregar y otro para eliminar. Nota: con la forma que se realizo se evita la mutabilidad.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,4 +16,14 @@ export class HomeComponent {
     'Crear Proyecto',
     'Crear Componentes'
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 }
